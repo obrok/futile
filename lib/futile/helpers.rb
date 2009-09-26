7b318d634afd73c3a0e0424fc84d77d74221f3d2
Michal Bugno <michal@bugno.net>
2009-09-26 17:37:49 +0200

    Moved some methods to session Small change to params_to_string

--- lib/futile/helpers.rb
+++ lib/futile/helpers.rb
@@ -1,41 +1,4 @@
 module Futile::Helpers
-  def fill(where, what)
-    # first find the only unique label
-    labels = response.parsed_body.search("//label[text()='%s']" % where)
-    if labels.size > 1
-      raise Futile::SearchIsFutile.new("Multiple labels found for '%s'" % [where])
-    end
-    label = labels.first
-    if label
-      # if the label was found locate element it was labeling
-      element = response.parsed_body.at("//input[@id='%s']" % [label["for"]])
-      element ||= response.parsed_body.at("//textarea[@id='%s']" % [label["for"]])
-    else
-      # else try to find element by name
-      element = response.parsed_body.at("//input[@name='%s']" % [where])
-      element ||= response.parsed_body.at("//textarea[@name='%s']" % [where])
-    end
-    raise Futile::SearchIsFutile.new("Cannot find '%s'" % [where]) unless element
-    if not (["text", "password"].include?(element["type"] || "text")) and element.name != "textarea"
-      # cannot type if the element is not a text field, password field or textarea
-      raise Futile::SearchIsFutile.new("Cannot type into '%s'" % [element])
-    end
-    if element["disabled"] or element["readonly"]
-      # cannot type into disabled/readonly field
-      raise Futile::SearchIsFutile.new("Element '%s' is disabled/readonly" % [element])
-    end
-    params[element["name"]] = what
-  end
-
-  def select(from, what)
-  end
-
-  def check(what)
-  end
-
-  def uncheck(what)
-  end
-
   def find_link(locator)
     #First try to treat it as a CSS or XPath locator
     #If that doesn't work search for a tag matching the specified text
@@ -48,6 +11,6 @@ module Futile::Helpers
 
   private
   def params_to_string
-    params.map { |k, v| "%s=%s" % [k, v] }.join("&")
+    params.map { |k, v| "%s=%s" % [k, v] }.sort.join("&")
   end
 end
--- lib/futile/session.rb
+++ lib/futile/session.rb
@@ -6,6 +6,7 @@ class Futile::Session
   def initialize(url, port, opts = {})
     @session = Net::HTTP.start(url, port)
     @max_redirects = opts[:max_redirects] || 10
+    reset_state
   end
 
   def get(uri)
@@ -43,6 +44,43 @@ class Futile::Session
 
   def disconnect
     @session.finish
+  end
+
+  def fill(where, what)
+    # first find the only unique label
+    labels = response.parsed_body.search("//label[text()='%s']" % where)
+    if labels.size > 1
+      raise Futile::SearchIsFutile.new("Multiple labels found for '%s'" % [where])
+    end
+    label = labels.first
+    if label
+      # if the label was found locate element it was labeling
+      element = response.parsed_body.at("//input[@id='%s']" % [label["for"]])
+      element ||= response.parsed_body.at("//textarea[@id='%s']" % [label["for"]])
+    else
+      # else try to find element by name
+      element = response.parsed_body.at("//input[@name='%s']" % [where])
+      element ||= response.parsed_body.at("//textarea[@name='%s']" % [where])
+    end
+    raise Futile::SearchIsFutile.new("Cannot find '%s'" % [where]) unless element
+    if not (["text", "password"].include?(element["type"] || "text")) and element.name != "textarea"
+      # cannot type if the element is not a text field, password field or textarea
+      raise Futile::SearchIsFutile.new("Cannot type into '%s'" % [element])
+    end
+    if element["disabled"] or element["readonly"]
+      # cannot type into disabled/readonly field
+      raise Futile::SearchIsFutile.new("Element '%s' is disabled/readonly" % [element])
+    end
+    params[element["name"]] = what
+  end
+
+  def select(from, what)
+  end
+
+  def check(what)
+  end
+
+  def uncheck(what)
   end
 
   private
