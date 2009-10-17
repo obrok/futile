b77095af95ef06d89d3ba2007c2cf65671acbfdf
Michal Bugno <michal@bugno.net>
2009-10-17 08:27:50 +0200

    Safari3 headers added user-agent header added

--- lib/futile/headers.rb
+++ lib/futile/headers.rb
@@ -8,29 +8,40 @@
 # @example Setting specific header
 #   headers[Futile::Headers::ACCEPT] = "*.*" # accept everything
 class Futile::Headers < Hash
-  ACCEPT = "accept".freeze
-  ACCEPT_LANGUAGE = "accept-language".freeze
+  ACCEPT          = "accept".freeze
+  ACCEPT_CHARSET  = "accept-charset".freeze
   ACCEPT_ENCODING = "accept-encoding".freeze
-  ACCEPT_CHARSET = "accept-charset".freeze
-  KEEP_ALIVE = "keep-alive".freeze
-  CONNECTION = "connection".freeze
+  ACCEPT_LANGUAGE = "accept-language".freeze
+  KEEP_ALIVE      = "keep-alive".freeze
+  CONNECTION      = "connection".freeze
+  USER_AGENT      = "user-agent".freeze
 
   REQUEST = {
     :firefox3 =>
     {
       ACCEPT          => "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
-      ACCEPT_LANGUAGE => "en-us,en;q=0.5",
-      ACCEPT_ENCODING => "gzip,deflate",
       ACCEPT_CHARSET  => "ISO-8859-1,utf-8;q=0.7,*;q=0.7",
-      KEEP_ALIVE      => "300",
+      ACCEPT_ENCODING => "gzip,deflate",
+      ACCEPT_LANGUAGE => "en-us,en;q=0.5",
       CONNECTION      => "keep-alive",
+      KEEP_ALIVE      => "300",
+      USER_AGENT      => "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.6; en-US; rv:1.9.1.3) Gecko/20090824 Firefox/3.5.3",
     },
+    :safari3 =>
+    {
+      ACCEPT          => "application/xml,application/xhtml+xml,text/html;q=0.9,text/plain;q=0.8,image/png,*/*;q=0.5",
+      ACCEPT_ENCODING => "gzip, deflate",
+      ACCEPT_LANGUAGE => "en-us",
+      CONNECTION      => "keep-alive",
+      USER_AGENT      => "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_1; en-us) AppleWebKit/531.9 (KHTML, like Gecko) Version/4.0.3 Safari/531.9",
+    }
   }.freeze
 
   ##
   # Sets request headers for specific _browser_. Available browsers can be
   # obtained with Futile::Headers::REQUEST.keys.
-  #
+  # @example Set headers for Safari 3
+  #   headers.browser = :safari3
   # @raise [Futile::ResistanceIsFutile] raised when browser is not found
   def browser=(browser)
     headers = REQUEST[browser]
--- test/headers_test.rb
+++ test/headers_test.rb
@@ -2,19 +2,28 @@ require "test/test_helper.rb"
 
 class HeadersTest < Futile::TestCase
   {
-    "host"            => "localhost:6666",
     "accept"          => "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
-    "accept-language" => "en-us,en;q=0.5",
-    "accept-encoding" => "gzip,deflate",
     "accept-charset"  => "ISO-8859-1,utf-8;q=0.7,*;q=0.7",
-    "keep-alive"      => "300",
+    "accept-encoding" => "gzip,deflate",
+    "accept-language" => "en-us,en;q=0.5",
     "connection"      => "keep-alive",
+    "host"            => "localhost:6666",
+    "keep-alive"      => "300",
+    "user-agent"      => "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.6; en-US; rv:1.9.1.3) Gecko/20090824 Firefox/3.5.3",
   }.each do |header, value|
-    define_method "test_header_#{header.gsub("-", "_")}".to_sym do
+    define_method "test_firefox3_request_header_#{header.gsub("-", "_")}".to_sym do
       @futile.get("/request_headers")
       headers = parse_response_headers(@futile.response.body)
       assert_header(headers, header, value)
     end
+  end
+
+  def test_changes_user_agent_header
+    @futile.headers.browser = :safari3
+    @futile.get("/request_headers")
+    headers = parse_response_headers(@futile.response.body)
+    assert_header(headers, "user-agent", "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_1; en-us) AppleWebKit/531.9 (KHTML, like Gecko) Version/4.0.3 Safari/531.9")
+    
   end
 
   def test_raises_when_browser_not_found
