7a05813554365f9550148df680aa2e9779203e7f
Michal Bugno <michal@bugno.net>
2009-10-21 09:06:20 +0200

    Structure of server

--- test/server/server.rb
+++ test/server/server.rb
@@ -15,20 +15,28 @@ class WebServer
     mount("/favicon.ico") { }
   end
 
+  def path_to(path)
+    File.join("test", "server", "root", path)
+  end
+
+  def layout(name)
+    ERB.new(File.read(File.join("test", "server", "layouts", name)))
+  end
+
   def page(path, content = nil, opts = {})
-    layout_name = opts[:layout] || "layout.erb"
+    layout_name = opts[:layout] || "layout.html.erb"
     mount(path) do |request, response|
       @request, @response = request, response
       yield request, response if block_given?
       if content.nil?
         # do nothing
       elsif content[-3 .. -1] == "erb"
-        layout = ERB.new(File.read(File.join("test", "server", "erb", layout_name)))
-        erb = ERB.new(File.read(File.join("test", "server", "erb", content)))
+        layout = layout(layout_name)
+        erb = ERB.new(File.read(path_to(content)))
         @content = erb.result(binding)
         response.body = layout.result(binding)
       else
-        response.body = File.read(File.join("test", "server", content))
+        response.body = File.read(path_to(content))
       end
     end
   end
@@ -52,19 +60,19 @@ class WebServer
 end
 
 server = WebServer.new
-server.page("/simple_get", "simple_html.erb")
-server.page("/second_page", "second_page.erb")
-server.page("/nested_path/index.html", "nested_path.erb")
-server.page("/nested_path/nested_in.html", "nested_path.erb")
-server.page("/form", "form.erb")
-server.page("/form_without_method", "form_without_method.erb")
-server.page("/scoped_links", "scoped_links.erb")
-server.page("/doit", "doit.erb")
-server.page("/scoped_links", "scoped_links.erb")
-server.page("/form_header", "form_header.erb")
-server.page("/request_headers", "request_headers.erb")
-server.page("/cookies", "cookies.erb")
-server.page("/500", "simple_html.erb") { |_, response| response.status = 500 }
+server.page("/simple_get", "simple_html.html.erb")
+server.page("/second_page", "second_page.html.erb")
+server.page("/nested_path/index.html", "nested_path.html.erb")
+server.page("/nested_path/nested_in.html", "nested_path.html.erb")
+server.page("/form", "form.html.erb")
+server.page("/form_without_method", "form_without_method.html.erb")
+server.page("/scoped_links", "scoped_links.html.erb")
+server.page("/doit", "doit.html.erb")
+server.page("/scoped_links", "scoped_links.html.erb")
+server.page("/form_header", "form_header.html.erb")
+server.page("/request_headers", "request_headers.html.erb")
+server.page("/cookies", "cookies.html.erb")
+server.page("/500", "simple_html.html.erb") { |_, response| response.status = 500 }
 server.page("/unknown_encoding") { |_, response| response["content-encoding"] = "nopez" }
 server.page("/gzipped_page", "gzipped_response.html.gz") { |_, response| response["content-encoding"] = "gzip" }
 server.page("/set_cookie") do |req, resp|
